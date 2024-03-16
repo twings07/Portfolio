32d39233b337a7123bb61e21330d0418b585e6fc
Edward Twingstan <89465559+twings07@users.noreply.github.com>
2024-03-15 23:06:20 -0400

    avancement du style et ajout contenu
    ajout background et liens
    ajout des projets + description

--- app.py
+++ app.py
@@ -21,17 +21,11 @@ Un finissant en multimédia qui s'intéresse au monde de la technologie, et pass
 Je suis à la recherche  d'un stage dans le domaine du jeux vidéo ou dans le web. 
 J’aime concevoir des modèles 3D. 
 """
-EMAIL = "twingstan2503@gmail.com"
-SOCIAL_MEDIA = {
-    "LinkedIn": "https://www.linkedin.com/in/edward-twingstan",
-    "GitHub": "https://https://github.com/twings07",
-    "Behance" : "https://www.behance.net/twingsEdward",
-}
-PROJECTS = {
-    "🏆 ",
-    "🏆 ",
-    "🏆 ",
-    "🏆 ",
+
+SOCIAL_MEDIA_ICONS = {
+    "LinkedIn": "assets/icons/linkedin.png",
+    "GitHub": "assets/icons/github.png",
+    "Behance": "assets/icons/behance.png",
 }
 
 st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
@@ -62,16 +56,25 @@ with col2:
       #  file_name=cvPdf.name,
        # mime="application/octet-stream",
    # )
-
-# --- Liens pour les reseaux sociaux ---
     
-cols = st.columns(len(SOCIAL_MEDIA)) # len sert a compter le nmb de colonne dans "social_media"
-for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
-    cols[index].write(f"[{platform}]({link})")
+col5, col6, col7 = st.columns(3)
+
+with col5:
+    st.markdown('<div style="display: flex; justify-content: center;"><a href="https://www.behance.net/twingsEdward"><img src="https://img.icons8.com/ffffff/behance"/></a></div>', unsafe_allow_html=True)
+
+with col6:
+    st.markdown('<div style="display: flex; justify-content: center;"><a href="https://www.linkedin.com/in/edward-twingstan"><img src="https://img.icons8.com/ffffff/linkedin.png"/></a></div>', unsafe_allow_html=True)
+
+with col7:
+    st.markdown('<div style="display: flex; justify-content: center;"><a href="https://github.com/twings07"><img src="https://img.icons8.com/ffffff/github"/></a></div>', unsafe_allow_html=True)
+
+st.write('\n')
+st.write('\n')
 
 # --- Compétences---
-st.write('\n')
+
 st.write("----")
+st.write('\n')
 st.subheader("Compétences")
 
 col3, col4 = st.columns(2, gap="small")
@@ -81,27 +84,37 @@ with col3:
         """
     <div class="vertical-list">
         <div class="hover-content">
-            <span class="title"> <p>- Conception de sites Web </p> </span>
+            <span class="title"> <p> ✔️ Conception de sites Web </p> </span>
             <div class="details">
-                <p> HTML, CSS, SQL, PHP </p>
+                <p> • HTML </p>
+                <p> • CSS </p>
+                <p> • SQL </p>
+                <p> • PHP </p>
             </div>
         </div>
          <div class="hover-content">
-            <span class="title"> <p>- Édition vidéo </p> </span>
+            <span class="title"> <p> ✔️ Édition vidéo </p> </span>
             <div class="details">
-                <p>Premiere Pro, After Effects</p>
+                <p>• Premiere Pro</p>
+                <p>• After Effects</p>
             </div>
         </div>
          <div class="hover-content">
-            <span class="title"> <p>- Montages vidéo </p> </span>
+            <span class="title"> <p> ✔️ Montages vidéo </p> </span>
             <div class="details">
-                <p>Premiere Pro, After Effects</p>
+                <p>• Premiere Pro</p>
+                <p>• After Effects</p>
             </div>
         </div>
          <div class="hover-content">
-            <span class="title"> <p>- Suite Adobe </p> </span>
+            <span class="title"> <p> ✔️ Suite Adobe </p> </span>
             <div class="details">
-                <p>Premiere Pro, After Effects, Photoshop, Lightroom, Illustrator, XD, Encoder </p>
+                <p>• Premiere Pro</p>
+                <p>• After Effects</p>
+                <p>• Photoshop</p>
+                <p>• Lightroom</p>
+                <p>• XD</p>
+                <p>• Encoder</p>
             </div>
         </div>
     </div>    
@@ -115,33 +128,42 @@ with col4:
         """
     <div class="vertical-list">
         <div class="hover-content">
-            <span class="title"> <p>- Conception de Jeux vidéo </p> </span>
+            <span class="title"> <p> ✔️ Conception de Jeux vidéo </p> </span>
             <div class="details">
-                <p> c#, Javascript, Python, Unity </p>
+                <p> • c# </p>
+                <p> • Javascript</p>
+                <p> • Python </p>
+                <p> • Unity </p>
             </div>
         </div>
          <div class="hover-content">
-            <span class="title"> <p>- Conception de modèles 3D </p> </span>
+            <span class="title"> <p> ✔️ Conception de modèles 3D </p> </span>
             <div class="details">
-                <p>Maya 3D, Mudbox</p>
+                <p> • Maya 3D</p>
+                 <p> • Mudbox</p>
             </div>
         </div>
          <div class="hover-content">
-            <span class="title"> <p>- Conception d’animations </p> </span>
+            <span class="title"> <p> ✔️ Conception d’animations </p> </span>
             <div class="details">
-                <p>Illustrator, Photoshop, Unity</p>
+                <p> • Illustrator </p>
+                <p> • Photoshop </p>
+                <p> • Unity </p>
             </div>
         </div>
          <div class="hover-content">
-            <span class="title"> <p>- Suite Autodesk </p> </span>
+            <span class="title"> <p> ✔️ Suite Autodesk </p> </span>
             <div class="details">
-                <p> Maya 3D, Mudbox </p>
+                <p> • Maya 3D</p>
+                 <p> • Mudbox</p>
             </div>
         </div>
         <div class="hover-content">
-            <span class="title"> <p>- Unity </p> </span>
+            <span class="title"> <p> ✔️ Unity </p> </span>
             <div class="details">
-                <p>c#, Unity 3D, Unity 2D </p>
+                <p> • c#</p>
+                <p> • Unity 3D </p>
+                <p> • Unity 2D </p>
             </div>
         </div>
         
@@ -191,16 +213,8 @@ with st.container():
         st.subheader("TicTacToe")
         st.write(
             """
-            Mon projet tictascvor fait en putdkjaslkdaj das
-            asd
-            adawdwadadjawd
-            a
-            dwaodawdwada
-            dakda  
-            daw
-            dawda
-            wdad
-            ad
+            J'ai développé un jeu de Tic Tac Toe personnalisé en utilisant Python et Pygame, 
+            où les joueurs utilisent deux images adorables de chats comme symboles. 
             """
         )
         st.markdown("[GitHub](https://github.com/twings07/TicTacToe)", unsafe_allow_html=True)
@@ -215,16 +229,8 @@ with st.container():
         st.subheader("Sudoku")
         st.write(
             """
-            Mon projet tictascvor fait en putdkjaslkdaj das
-            asd
-            adawdwadadjawd
-            a
-            dwaodawdwada
-            dakda  
-            daw
-            dawda
-            wdad
-            ad
+           J'ai développé un jeu de Sudoku en utilisant Python avec l'aide de Pygame et de la bibliothèque Dokusan, qui présente un défi classique de Sudoku.
+            Le jeu utilise un algorithme de backtracking pour résoudre les planches de Sudoku.
             """
         )
         st.markdown("[GitHub](https://github.com/twings07/Sudoku)", unsafe_allow_html=True)
